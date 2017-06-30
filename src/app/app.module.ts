a430e4d84be0b1f7554579728d2bde1c69bb9884
aayanqazi <aayanqazi@yahoo.com>
2017-06-30 05:17:05 +0500

    Nesting Components and Inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
+import { JokeListComponent } from './joke-list/joke-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent,
+    JokeListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/joke/joke.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-joke',
+  templateUrl: './joke.component.html',
+  styleUrls: ['./joke.component.css']
+})
+export class JokeComponent implements OnInit {
+  @Input('joke') data: Joke;  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+class Joke {
+  public setup: string;
+  public punchline: string;
+  public hide: boolean;
+
+  constructor(setup: string, punchline: string) {
+    this.setup = setup;
+    this.punchline = punchline;
+    this.hide = true;
+  }
+
+  toggle() {
+    this.hide = !this.hide;
+  }
+}
