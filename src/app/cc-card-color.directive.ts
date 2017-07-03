19a83de556b5e864dcaf81cc48d25a1fe176a96c
root <root@localhost.localdomain>
2017-07-03 20:28:03 +0500

    Learned host Listener and host Binding

--- src/app/cc-card-color.directive.ts
+++ src/app/cc-card-color.directive.ts
@@ -1,14 +1,24 @@
-import { Directive ,ElementRef , Renderer} from '@angular/core';
+import { Directive ,ElementRef , Renderer,HostListener,HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appCcCardColor]'
 })
 export class CcCardColorDirective {
-
+  @HostBinding('class.card-outline-primary') private isHovering = false;
   constructor(private el:ElementRef , private renderer:Renderer) {
-    renderer.setElementStyle(el.nativeElement,'backgroundColor','grey');
-    renderer.setElementStyle(el.nativeElement,'color','white');
-
+    // renderer.setElementStyle(el.nativeElement,'backgroundColor','grey');
+    // renderer.setElementStyle(el.nativeElement,'color','white');
   }
 
+  @HostListener('mouseover') onMouseOver(){
+    let part = this.el.nativeElement.querySelector('.card-text');
+    this.renderer.setElementStyle(part,'display','block');
+    this.isHovering = true;
+  }
+
+    @HostListener('mouseleave') onMouseLeave(){
+    let part = this.el.nativeElement.querySelector('.card-text');
+    this.renderer.setElementStyle(part,'display','none');
+    this.isHovering = false;
+  }
 }
