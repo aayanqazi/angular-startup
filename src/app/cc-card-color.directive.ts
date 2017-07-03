dc23f93ed50d4a8f121f65f9b01d069b413240f7
aayanqazi@yahoo.com <aayanqazi>
2017-07-03 20:37:55 +0500

    Config Parameter directives

--- src/app/cc-card-color.directive.ts
+++ src/app/cc-card-color.directive.ts
@@ -1,9 +1,12 @@
-import { Directive ,ElementRef , Renderer,HostListener,HostBinding} from '@angular/core';
+import { Directive ,ElementRef ,Input, Renderer,HostListener,HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appCcCardColor]'
 })
 export class CcCardColorDirective {
+  @Input('appCcCardColor') config:any = {
+    querySelector:''
+  };
   @HostBinding('class.card-outline-primary') private isHovering = false;
   constructor(private el:ElementRef , private renderer:Renderer) {
     // renderer.setElementStyle(el.nativeElement,'backgroundColor','grey');
@@ -11,13 +14,13 @@ export class CcCardColorDirective {
   }
 
   @HostListener('mouseover') onMouseOver(){
-    let part = this.el.nativeElement.querySelector('.card-text');
+    let part = this.el.nativeElement.querySelector(this.config.querySelector);
     this.renderer.setElementStyle(part,'display','block');
     this.isHovering = true;
   }
 
     @HostListener('mouseleave') onMouseLeave(){
-    let part = this.el.nativeElement.querySelector('.card-text');
+    let part = this.el.nativeElement.querySelector(this.config.querySelector);
     this.renderer.setElementStyle(part,'display','none');
     this.isHovering = false;
   }
